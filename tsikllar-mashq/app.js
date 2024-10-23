6a95fc3cefbf6a32829d92c5684e19eee7393642
AsliddinMamadiyev <asliddinmamadiyev006@gmail.com>
2024-10-23 16:17:39 +0500

    first commit

---
+++ tsikllar-mashq/app.js
@@ -0,0 +1,95 @@
+"use strict";
+
+// ---1----
+
+// for(let i = 0; i <= 10; i++) {
+//     console.log(i);    
+// }
+
+
+
+// let i = 0;
+
+// while(i <= 10) {
+//     console.log(i);
+//     i++;
+// }
+
+
+
+// let i = 0;
+
+// do {
+//     console.log(i);
+//     i++;
+// } while(i <= 10);
+
+
+// ---2----
+// for(let i = 10; i >= 0; i--) {
+//     console.log(i);    
+// }
+
+
+// let i = 10;
+
+// while(i >= 0) {
+//     console.log(i);
+//     i--;
+// }
+
+// let i = 10;
+
+// do {
+//     console.log(i);
+//     i--;
+// } while(i >= 0);
+
+// ---3----
+
+// for(let i = 0; i < n; i++) {
+//     console.log(i);
+// }
+
+// ---4----
+
+// for (let i = 1; i <= 10; i++) {
+//         console.log(`${i} x ${i} = ${i * i}`);
+//     }
+//     console.log();
+// }
+
+
+
+// let i = 1;
+
+// while(i <= 10) {
+//     console.log('${i} x ${i} = ${i * i}');
+//     i++;
+// }
+
+
+// let i = 1;
+
+// do {
+//     console.log(`${i} x ${i} = ${i * i}`);
+//     i++;
+// } while(i <= 10);
+
+// ---5----
+
+// for(let i = 0; i < 100; i += 2) {
+//     console.log(i);
+// }
+
+// ---6----
+
+// for(let i = 1; i < 100; i += 2) {
+//     console.log(i);
+// }
+
+// ---7----
+
+// for(let i = 1; i <= 100; i %= 2) {
+//     console.log(i);
+// }
